16ec873c9fe9aa3da3a73f28a1b562a3519c0dc4
Evan Allan <evanallan1026@gmail.com>
2016-08-06 20:46:05 -0400

    This really should be a namespace

--- src/Utils/Math.h
+++ src/Utils/Math.h
@@ -3,25 +3,23 @@
 
 #include <type_traits>
 
-class Math {
-    public:
+namespace Math {
+    // Template signum implementation from
+    // http://stackoverflow.com/questions/1903954/is-there-a-standard-sign-function-signum-sgn-in-c-c
+    template <typename T> inline constexpr
+    static int signum(T x, std::false_type is_signed) {
+        return T(0) < x;
+    }
 
-        // Template signum implementation from
-        // http://stackoverflow.com/questions/1903954/is-there-a-standard-sign-function-signum-sgn-in-c-c
-        template <typename T> inline constexpr
-        static int signum(T x, std::false_type is_signed) {
-            return T(0) < x;
-        }
+    template <typename T> inline constexpr
+    static int signum(T x, std::true_type is_signed) {
+        return (T(0) < x) - (x < T(0));
+    }
 
-        template <typename T> inline constexpr
-        static int signum(T x, std::true_type is_signed) {
-            return (T(0) < x) - (x < T(0));
-        }
-
-        template <typename T> inline constexpr
-        static int signum(T x) {
-            return signum(x, std::is_signed<T>());
-        }
+    template <typename T> inline constexpr
+    static int signum(T x) {
+        return signum(x, std::is_signed<T>());
+    }
 };
 
 #endif
