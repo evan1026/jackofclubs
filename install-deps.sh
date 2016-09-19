7c6b2ad15b43cfcbe1c44dabdf7d0600604c21a8
Evan Allan <evanallan1026@gmail.com>
2016-09-19 11:32:13 -0400

    Apparently linux doesn't come with g++

--- install-deps.sh
+++ install-deps.sh
@@ -1,4 +1,4 @@
-libs="cmake git libfreetype6-dev libjpeg-dev libx11-dev libxrandr-dev libxcb-present-dev libx11-xcb-dev libxcb-randr0-dev libxcb-image0-dev libgl1-mesa-dev libflac-dev libogg-dev libvorbis-dev libvorbisenc2 libvorbisfile3 libopenal-dev libpthread-stubs0-dev"
+libs="g++ cmake git libfreetype6-dev libjpeg-dev libx11-dev libxrandr-dev libxcb-present-dev libx11-xcb-dev libxcb-randr0-dev libxcb-image0-dev libgl1-mesa-dev libflac-dev libogg-dev libvorbis-dev libvorbisenc2 libvorbisfile3 libopenal-dev libpthread-stubs0-dev"
 sudo apt-get install $libs
 
 tmpdir=$(mktemp -d)
